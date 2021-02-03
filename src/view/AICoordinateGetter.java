8ea89c8fba7c2ddb9feeb06eaced7a8b3157b829
Relmen19 <andrew.avramencko2104@gmail.com>
2021-02-03 18:55:54 +0300

    исправлена логика для AI бота, но он все равно падает)))

--- src/view/AICoordinateGetter.java
+++ src/view/AICoordinateGetter.java
@@ -5,43 +5,43 @@ import model.Figure;
 import model.Point;
 import model.exceptions.InvalidPointException;
 
-public class AICoordinateGetter implements ICoordinateGetter {
+public class AICoordinateGetter<privte> implements ICoordinateGetter {
 
     @Override
     public Point coordinateGetter(final Field field) throws InvalidPointException {
         if(field.getFigure(new Point(1, 1)) == null) return new Point(1, 1);
 
         if(field.getFigure(new Point(0, 0)) == null && field.getFigure(new Point(2, 2)) == null) return new Point(0, 0);
-        else if(field.getFigure(new Point(0, 0)) == Figure.X && field.getFigure(new Point(2, 2)) == null) return new Point(2, 2);
-        else if (field.getFigure(new Point(0, 0)) == Figure.X && field.getFigure(new Point(2, 2)) == Figure.O) {
+        else if(field.getFigure(new Point(0, 0)) == Figure.O && field.getFigure(new Point(2, 2)) == null) return new Point(2, 2);
+        else if (field.getFigure(new Point(0, 0)) == Figure.O && field.getFigure(new Point(2, 2)) == Figure.X) {
 
-            if(field.getFigure(new Point(0, 2)) == Figure.O && field.getFigure(new Point(2, 0)) == null) {
+            if(field.getFigure(new Point(0, 2)) == Figure.X && field.getFigure(new Point(2, 0)) == null) {
                 if(field.getFigure(new Point(1, 2)) == null) return new Point(1, 2);
                 else if(field.getFigure(new Point(1, 0)) == null) return new Point(1, 0);
-                else if(field.getFigure(new Point(1, 0)) == Figure.O) {
+                else if(field.getFigure(new Point(1, 0)) == Figure.X) {
                     if(field.getFigure(new Point(2, 1)) == null) return new Point(2, 1);
                     else if(field.getFigure(new Point(0, 1)) == null) return new Point(0, 1);
-                    else if(field.getFigure(new Point(0, 1)) == Figure.O) return new Point(2, 0);
+                    else if(field.getFigure(new Point(0, 1)) == Figure.X) return new Point(2, 0);
                 }
             }
 
-            if(field.getFigure(new Point(2, 0)) == Figure.O && field.getFigure(new Point(0, 2)) == null){
+            if(field.getFigure(new Point(2, 0)) == Figure.X && field.getFigure(new Point(0, 2)) == null){
                 if(field.getFigure(new Point(2, 1)) == null) return new Point(2, 1);
                 else if(field.getFigure(new Point(0, 1)) == null) return new Point(0, 1);
-                else if(field.getFigure(new Point(0, 1)) == Figure.O) {
+                else if(field.getFigure(new Point(0, 1)) == Figure.X) {
                     if(field.getFigure(new Point(1, 2)) == null) return new Point(1, 2);
                     else if(field.getFigure(new Point(1, 0)) == null) return new Point(1, 0);
-                    else if(field.getFigure(new Point(1, 0)) == Figure.O) return new Point(0, 1);
+                    else if(field.getFigure(new Point(1, 0)) == Figure.X) return new Point(0, 1);
                 }
             }
 
-            if(field.getFigure(new Point(1, 2)) == Figure.O || field.getFigure(new Point(1, 0)) == Figure.O) {
+            if(field.getFigure(new Point(1, 2)) == Figure.X || field.getFigure(new Point(1, 0)) == Figure.X) {
                 if(field.getFigure(new Point(0, 2)) == null) return new Point(0, 2);
                 if(field.getFigure(new Point(0, 1)) == null) return new Point(0, 1);
                 if(field.getFigure(new Point(2, 0)) == null) return new Point(2, 0);
             }
 
-            if(field.getFigure(new Point(2, 1)) == Figure.O || field.getFigure(new Point(0, 1)) == Figure.O) {
+            if(field.getFigure(new Point(2, 1)) == Figure.X || field.getFigure(new Point(0, 1)) == Figure.X) {
                 if(field.getFigure(new Point(2, 0)) == null) return new Point(2, 0);
                 if(field.getFigure(new Point(1, 0)) == null) return new Point(1, 0);
                 if(field.getFigure(new Point(0, 2)) == null) return new Point(0, 2);
@@ -50,36 +50,36 @@ public class AICoordinateGetter implements ICoordinateGetter {
 
 
         if(field.getFigure(new Point(0, 2)) == null && field.getFigure(new Point(2, 0)) == null) return new Point(0, 2);
-        else if(field.getFigure(new Point(0, 2)) == Figure.X && field.getFigure(new Point(2, 0)) == null) return new Point(2, 0);
-        else if(field.getFigure(new Point(0, 2)) == Figure.X && field.getFigure(new Point(2, 0)) == Figure.O) {
+        else if(field.getFigure(new Point(0, 2)) == Figure.O && field.getFigure(new Point(2, 0)) == null) return new Point(2, 0);
+        else if(field.getFigure(new Point(0, 2)) == Figure.O && field.getFigure(new Point(2, 0)) == Figure.X) {
 
-            if(field.getFigure(new Point(2, 2)) == Figure.O && field.getFigure(new Point(0, 0)) == null) {
+            if(field.getFigure(new Point(2, 2)) == Figure.X && field.getFigure(new Point(0, 0)) == null) {
                 if(field.getFigure(new Point(2, 1)) == null) return new Point(2, 1);
                 else if(field.getFigure(new Point(0, 1)) == null) return new Point(0, 1);
-                else if(field.getFigure(new Point(0, 1)) == Figure.O) {
+                else if(field.getFigure(new Point(0, 1)) == Figure.X) {
                     if(field.getFigure(new Point(1, 0)) == null) return new Point(1, 0);
                     else if(field.getFigure(new Point(1, 2)) == null) return new Point(1, 2);
-                    else if(field.getFigure(new Point(1, 2)) == Figure.O) return new Point(0, 0);
+                    else if(field.getFigure(new Point(1, 2)) == Figure.X) return new Point(0, 0);
                 }
             }
 
-            if(field.getFigure(new Point(0, 0)) == Figure.O && field.getFigure(new Point(2, 2)) == null){
+            if(field.getFigure(new Point(0, 0)) == Figure.X && field.getFigure(new Point(2, 2)) == null){
                 if(field.getFigure(new Point(1, 0)) == null) return new Point(1, 0);
                 else if(field.getFigure(new Point(1, 2)) == null) return new Point(1, 2);
-                else if(field.getFigure(new Point(1, 2)) == Figure.O) {
+                else if(field.getFigure(new Point(1, 2)) == Figure.X) {
                     if(field.getFigure(new Point(2, 1)) == null) return new Point(2, 1);
                     else if(field.getFigure(new Point(0, 1)) == null) return new Point(0, 1);
-                    else if(field.getFigure(new Point(0, 1)) == Figure.O) return new Point(2, 2);
+                    else if(field.getFigure(new Point(0, 1)) == Figure.X) return new Point(2, 2);
                 }
             }
 
-            if(field.getFigure(new Point(1, 2)) == Figure.O || field.getFigure(new Point(1, 0)) == Figure.O) {
+            if(field.getFigure(new Point(1, 2)) == Figure.X || field.getFigure(new Point(1, 0)) == Figure.X) {
                 if(field.getFigure(new Point(0, 0)) == null) return new Point(0, 0);
                 if(field.getFigure(new Point(0, 1)) == null) return new Point(0, 1);
                 if(field.getFigure(new Point(2, 2)) == null) return new Point(2, 2);
             }
 
-            if(field.getFigure(new Point(2, 1)) == Figure.O || field.getFigure(new Point(0, 1)) == Figure.O) {
+            if(field.getFigure(new Point(2, 1)) == Figure.X || field.getFigure(new Point(0, 1)) == Figure.X) {
                 if(field.getFigure(new Point(2, 2)) == null) return new Point(2, 2);
                 if(field.getFigure(new Point(1, 2)) == null) return new Point(1, 2);
                 if(field.getFigure(new Point(0, 0)) == null) return new Point(0, 0);
@@ -87,5 +87,8 @@ public class AICoordinateGetter implements ICoordinateGetter {
         }
         return new Point(1, 1);
     }
+
+
+//    private
 
 }
